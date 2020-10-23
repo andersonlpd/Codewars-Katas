5abc35a90083e6f9d796ed0651a5223a82e3986f
Anderson Dorigão <34915327+andersonlpd@users.noreply.github.com>
2020-10-23 13:39:45 -0300

    Update Persistent-Bugger.py

--- python/6-kyu/Persistent-Bugger/Persistent-Bugger.py
+++ python/6-kyu/Persistent-Bugger/Persistent-Bugger.py
@@ -1,19 +1,12 @@
 import numpy 
 def persistence(n):
-    
     toList = list(str(n))
-       
     SplittedNums = numpy.array(toList).astype(numpy.integer)
-    
     Length = len(SplittedNums)
-    
     Count = 0
-    
     while Length > 1:
         Product = list(str(numpy.prod(SplittedNums)))
         SplittedNums = numpy.array(Product).astype(numpy.integer)
         Length = len(Product)
         Count += 1
-        
     return Count
-        
